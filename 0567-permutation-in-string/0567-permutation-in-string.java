3f51bd3b08100a71d2eb0f25a8479b61557b4000
Shashank <139386976+Shashanklko@users.noreply.github.com>
2026-07-21 02:43:04 +0530

    Time: 6 ms (87.84%), Space: 44 MB (61.72%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-
         int m = s1.length();
         int n = s2.length();
-
-        if (m > n) {
-            return false;
+        if(m>n) return false;
+        int[] target = new int[26];
+        int[] window = new int[26];
+        for(int i = 0 ; i<m;i++){
+            target[s1.charAt(i)-'a']++;
+            window[s2.charAt(i)-'a']++; 
         }
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        // Frequency of s1 and first window of s2
-        for (int i = 0; i < m; i++) {
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
-        }
-
-        // Check first window
-        if (Arrays.equals(freq1, freq2)) {
+        if(Arrays.equals(target , window)){
             return true;
         }
-
-        // Slide the window
-        for (int right = m; right < n; right++) {
-
-            // Add new character
-            freq2[s2.charAt(right) - 'a']++;
-
-            // Remove old character
-            freq2[s2.charAt(right - m) - 'a']--;
-
-            if (Arrays.equals(freq1, freq2)) {
-                return true;
+        for(int right = m ; right<n;right++){
+            window[s2.charAt(right)-'a']++;
+            window[s2.charAt(right-m)-'a']--;
+            if(Arrays.equals(target , window)){
+            return true;
             }
         }
-
         return false;
     }
 }
