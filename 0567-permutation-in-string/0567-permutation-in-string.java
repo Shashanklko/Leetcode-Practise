5137b379d91f8f8bdcb3fc175498858f04c8bd14
Shashank <139386976+Shashanklko@users.noreply.github.com>
2026-07-21 00:59:34 +0530

    Time: 6 ms (87.74%), Space: 44.3 MB (37.98%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -8,15 +8,30 @@ class Solution {
             return false;
         }
 
-        char[] target = s1.toCharArray();
-        Arrays.sort(target);
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
-        for (int i = 0; i <= n - m; i++) {
+        // Frequency of s1 and first window of s2
+        for (int i = 0; i < m; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
 
-            char[] window = s2.substring(i, i + m).toCharArray();
-            Arrays.sort(window);
+        // Check first window
+        if (Arrays.equals(freq1, freq2)) {
+            return true;
+        }
 
-            if (Arrays.equals(target, window)) {
+        // Slide the window
+        for (int right = m; right < n; right++) {
+
+            // Add new character
+            freq2[s2.charAt(right) - 'a']++;
+
+            // Remove old character
+            freq2[s2.charAt(right - m) - 'a']--;
+
+            if (Arrays.equals(freq1, freq2)) {
                 return true;
             }
         }
