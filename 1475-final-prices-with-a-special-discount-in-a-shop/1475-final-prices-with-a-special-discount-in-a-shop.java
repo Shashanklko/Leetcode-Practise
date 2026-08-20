0ebbe191677477c49d1779ec1c6fb40687fcbfa6
Shashank <139386976+Shashanklko@users.noreply.github.com>
2026-08-21 03:05:22 +0530

    Time: 4 ms (50.49%), Space: 45.5 MB (23.42%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         Stack<Integer> st = new Stack<>();
-        int[] arr = new int[prices.length];
+        int[] arr = prices.clone();
         for(int i =  0 ; i<prices.length;i++){
             while(!st.isEmpty() && prices[st.peek()]>=prices[i]){
                 int top = st.pop();
@@ -9,11 +9,6 @@ class Solution {
             }
             st.push(i);
         }
-        while (!st.isEmpty()) {
-            int top = st.pop();
-            arr[top] = prices[top];
-        }
-
         return arr;
     }
 }
